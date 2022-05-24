fe11cd74de121a9ea5562ae3a13b2014c7450dcc
badjateamisha <amishabadjate175@gmail.com>
2022-05-24 09:42:02 +0530

    Using default and parameterised constructor

--- MoodAnalyzer/Program.cs
+++ MoodAnalyzer/Program.cs
@@ -1,9 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
 using MoodAnalyzer;
 
 Console.WriteLine("Welcome to Mood Analyser!");
 
 
-AnalyseMood analyseMood = new();
-string result = analyseMood.AnalyseMoodMethod("I am in happy mood");
-Console.WriteLine(result);
+AnalyseMood A1 = new();
+string result1 = A1.AnalyseMoodMethod("I am in Sad mood");
+Console.WriteLine(result1);
+
+AnalyzeMoodUsingConstructor A2 = new();
+string result2 = A2.AnalyseMoodMethod();
+Console.WriteLine("Using the Default Constructor here and Mood is :" + result2);
+
+AnalyzeMoodUsingConstructor A3 = new("I am i Happy Mood");
+string result3 = A3.AnalyseMoodMethod();
+Console.WriteLine("Using the Parameterized Constructor here and Mood is :" + result3);
